de895e61bcc95111d1463fddf029e6d02ec18e90
upjdesign <38765631+upjdesign@users.noreply.github.com>
2021-07-12 22:18:29 -0300

    Em fazer de desenvolvimento

--- js/interacoes.js
+++ js/interacoes.js
@@ -1,1 +1,27 @@
 //Começa tudo aqui
+
+const tarefasInput         = document.querySelector("#tarefasText");
+const botaoCadastrarTarefa = document.querySelector("#botao-cadastrar");
+var valoresTarefas         = document.querySelector("#valoresTarefas tbody");
+var quantidade             = 0;
+
+botaoCadastrarTarefa.onclick = () => {
+    var ultimoValor               = localStorage.length;
+    quantidade = (ultimoValor > quantidade) ? ultimoValor + 1 : quantidade;
+    
+    var dadosTarefa               = tarefasInput.value;
+    localStorage.setItem(quantidade,dadosTarefa);
+    var valoresTrs = '<tr> <td>'+localStorage.key(0)+'</td> <td>'+ localStorage.getItem(0) +'</td> <td>+</td> <td>-</td> </tr>';
+    
+    for(var i = 0; i < ultimoValor; i++){
+       valoresTrs += '<tr> <td>'+localStorage.key(i)+'</td> <td>'+ localStorage.getItem((ultimoValor < 1) ? 0 : ultimoValor) +'</td> <td>+</td> <td>-</td> </tr>';
+    }
+    
+    valoresTarefas.innerHTML=valoresTrs;
+    quantidade++;
+
+}
+
+
+
+
