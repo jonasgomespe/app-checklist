4113e9f17701fffdfeb598d8a8ac48900e7665c1
upjdesign <38765631+upjdesign@users.noreply.github.com>
2021-07-17 21:36:59 -0300

    Foi criado duas funçõs uma para manipular o scroll

--- js/interacoes.js
+++ js/interacoes.js
@@ -2,29 +2,32 @@
 
 const tarefasInput         = document.querySelector("#tarefasText");
 const botaoCadastrarTarefa = document.querySelector("#botao-cadastrar");
+const divBarraRolagem      = document.querySelector(".container-lista div");
 var valoresTarefas         = document.querySelector("#valoresTarefas tbody");
 var quantidade             = 0;
 var valoresTrs             = "";
 var ultimoValor            = localStorage.length;
 
-if(localStorage.getItem){
+function scrollMove(){
+    divBarraRolagem.scrollTo(0,divBarraRolagem.querySelector("table").clientHeight);
+}
+
+if(localStorage.getItem(0)){
     for( var i = 0; i < ultimoValor; i++ ){
         valoresTarefas.innerHTML += '<tr> <td>'+ i +'</td> <td>'+ localStorage.getItem(i) +'</td> <td>+</td> <td>-</td> </tr>';    
     }
     quantidade = ultimoValor;
+    scrollMove();
 }
 
 botaoCadastrarTarefa.onclick = () => {
-    //quantidade = (ultimoValor > quantidade) ? ultimoValor + 1 : quantidade;
     var dadosTarefa               = tarefasInput.value;
     localStorage.setItem(quantidade,dadosTarefa);
-    //valoresTrs += '<tr> <td>'+localStorage.key(quantidade)+'</td> <td>'+ localStorage.getItem(quantidade) +'</td> <td>+</td> <td>-</td> </tr>';
     
     valoresTarefas.innerHTML += '<tr> <td>'+ quantidade +'</td> <td>'+ localStorage.getItem(quantidade) +'</td> <td>+</td> <td>-</td> </tr>';
     quantidade++;
+    scrollMove();
 }
-//console.log(valoresTrs);  
-
 
 
 
